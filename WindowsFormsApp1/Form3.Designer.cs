51c25500063bc9654104e92f193a79d98d0c51f9
Domingo Carrasco Bueno <domi1213@hotmail.com>
2019-04-12 13:29:57 +0200

    Incluimos insert tipe product
    incluimos insert tipe product
    necesitamos modificar vista y los count de los productos para visualizar correctamente.
    
    Las salidas ya sería genial
    
    Posibilidad de exportar sql de casa aquí o al reves

--- WindowsFormsApp1/Form3.Designer.cs
+++ WindowsFormsApp1/Form3.Designer.cs
@@ -84,7 +84,7 @@
             // 
             // textPeso
             // 
-            this.textPeso.Location = new System.Drawing.Point(258, 215);
+            this.textPeso.Location = new System.Drawing.Point(307, 215);
             this.textPeso.Name = "textPeso";
             this.textPeso.Size = new System.Drawing.Size(100, 20);
             this.textPeso.TabIndex = 6;
@@ -92,7 +92,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(270, 199);
+            this.label4.Location = new System.Drawing.Point(321, 199);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(71, 13);
             this.label4.TabIndex = 7;
@@ -100,7 +100,7 @@
             // 
             // textCantidad
             // 
-            this.textCantidad.Location = new System.Drawing.Point(138, 215);
+            this.textCantidad.Location = new System.Drawing.Point(177, 215);
             this.textCantidad.Name = "textCantidad";
             this.textCantidad.Size = new System.Drawing.Size(100, 20);
             this.textCantidad.TabIndex = 10;
@@ -109,7 +109,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(160, 199);
+            this.label6.Location = new System.Drawing.Point(207, 199);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(48, 13);
             this.label6.TabIndex = 11;
@@ -118,7 +118,7 @@
             // 
             // buttonSave
             // 
-            this.buttonSave.Location = new System.Drawing.Point(436, 241);
+            this.buttonSave.Location = new System.Drawing.Point(487, 241);
             this.buttonSave.Name = "buttonSave";
             this.buttonSave.Size = new System.Drawing.Size(118, 55);
             this.buttonSave.TabIndex = 12;
@@ -136,7 +136,7 @@
             // 
             // textBoxInfo
             // 
-            this.textBoxInfo.Location = new System.Drawing.Point(377, 35);
+            this.textBoxInfo.Location = new System.Drawing.Point(436, 39);
             this.textBoxInfo.Multiline = true;
             this.textBoxInfo.Name = "textBoxInfo";
             this.textBoxInfo.Size = new System.Drawing.Size(209, 200);
@@ -146,7 +146,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(433, 9);
+            this.label7.Location = new System.Drawing.Point(497, 23);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(108, 13);
             this.label7.TabIndex = 18;
@@ -155,7 +155,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(171, 253);
+            this.label8.Location = new System.Drawing.Point(237, 262);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(106, 13);
             this.label8.TabIndex = 21;
@@ -164,7 +164,7 @@
             // LabelPesoMedio
             // 
             this.LabelPesoMedio.AutoSize = true;
-            this.LabelPesoMedio.Location = new System.Drawing.Point(202, 283);
+            this.LabelPesoMedio.Location = new System.Drawing.Point(258, 283);
             this.LabelPesoMedio.Name = "LabelPesoMedio";
             this.LabelPesoMedio.Size = new System.Drawing.Size(53, 13);
             this.LabelPesoMedio.TabIndex = 22;
@@ -173,7 +173,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(295, 135);
+            this.label9.Location = new System.Drawing.Point(347, 136);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(28, 13);
             this.label9.TabIndex = 23;
@@ -181,14 +181,14 @@
             // 
             // textBoxLote
             // 
-            this.textBoxLote.Location = new System.Drawing.Point(258, 151);
+            this.textBoxLote.Location = new System.Drawing.Point(307, 152);
             this.textBoxLote.Name = "textBoxLote";
             this.textBoxLote.Size = new System.Drawing.Size(100, 20);
             this.textBoxLote.TabIndex = 24;
             // 
             // textBoxPrecio
             // 
-            this.textBoxPrecio.Location = new System.Drawing.Point(138, 151);
+            this.textBoxPrecio.Location = new System.Drawing.Point(177, 152);
             this.textBoxPrecio.Name = "textBoxPrecio";
             this.textBoxPrecio.Size = new System.Drawing.Size(100, 20);
             this.textBoxPrecio.TabIndex = 25;
@@ -196,7 +196,7 @@
             // labelPrecio
             // 
             this.labelPrecio.AutoSize = true;
-            this.labelPrecio.Location = new System.Drawing.Point(171, 135);
+            this.labelPrecio.Location = new System.Drawing.Point(218, 136);
             this.labelPrecio.Name = "labelPrecio";
             this.labelPrecio.Size = new System.Drawing.Size(37, 13);
             this.labelPrecio.TabIndex = 26;
@@ -205,15 +205,15 @@
             // comboBoxTipo
             // 
             this.comboBoxTipo.FormattingEnabled = true;
-            this.comboBoxTipo.Location = new System.Drawing.Point(12, 179);
+            this.comboBoxTipo.Location = new System.Drawing.Point(12, 151);
             this.comboBoxTipo.Name = "comboBoxTipo";
-            this.comboBoxTipo.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxTipo.Size = new System.Drawing.Size(159, 21);
             this.comboBoxTipo.TabIndex = 27;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(38, 160);
+            this.label1.Location = new System.Drawing.Point(38, 135);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(28, 13);
             this.label1.TabIndex = 28;
@@ -223,7 +223,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(598, 310);
+            this.ClientSize = new System.Drawing.Size(664, 310);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBoxTipo);
             this.Controls.Add(this.labelPrecio);
